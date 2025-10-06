456e1d7f523ecc1f62673d9bdba2ce34ef85d906
NXDuyThang <tavanangg@gmai.com>
2025-10-06 23:06:07 +0700

    sua lai vung

--- main.tf
+++ main.tf
@@ -6,19 +6,24 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "my-terraform-state-bucket-demo"
-    key    = "terraform/state.tfstate"
-    region = "ap-southeast-1"
-  }
+  # Tạm thời tắt backend nếu chưa có S3 state bucket
+  # backend "s3" {
+  #   bucket = "my-terraform-state-bucket-demo"
+  #   key    = "terraform/state.tfstate"
+  #   region = "ap-southeast-1"
+  # }
 }
 
 provider "aws" {
   region = "ap-southeast-1"
 }
 
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "my-demo-gitops-bucket-123456"
+  bucket = "my-demo-gitops-bucket-${random_id.suffix.hex}"
   tags = {
     Name = "GitOpsDemo"
   }
